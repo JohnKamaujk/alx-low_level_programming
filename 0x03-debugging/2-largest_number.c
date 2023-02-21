a2ffc38b4d133a239120581e99c235ad96aa090e
JohnKamaujk <johnjkkamau@gmail.com>
2023-02-22 00:28:18 +0300

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
+ * a: first integer
+ * b: second integer
+ * c: third integer
  * Return: largest number
  */
 

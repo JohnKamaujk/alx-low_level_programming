dd5897fb28bb2b975b360049af14e5d8c04e2401
JohnKamaujk <johnjkkamau@gmail.com>
2023-03-01 16:27:20 +0300

    pointers arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,31 @@
+
 #include "main.h"
 
 /**
  * _strcat - concatenates two strings
- * @first: string to append to
- * @add: string to add
+ * @dest: string to append to
+ * @src: string to add
  *
  * Return: a pointer to the resulting string
  */
-char *_strcat(char *first, char *add)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
 	i = 0;
 	j = 0;
 
-	while (first[i] != '\0')
+	while (dest[i] != '\0')
 		i++;
 
-	while (add[j] != '\0')
+	while (src[j] != '\0')
 	{
-		first[i] = add[j];
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
 
-	first[i] = '\0';
+	dest[i] = '\0';
 
-	return (first);
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,30 +2,30 @@
 
 /**
  * _strncat - concatenates n bytes from a string to another
- * @first: destination string
+ * @dest: destination string
  * @src: source string
  * @n: number of bytes of str to concatenate
  *
  * Return: a pointer to the resulting string dest
  */
-char *_strncat(char *first, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = 0;
 	j = 0;
 
-	while (first[i] != '\0')
+	while (dest[i] != '\0')
 		i++;
 
 	while (src[j] != '\0' && j < n)
 	{
-		first[i] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	first[i] = '\0';
+	dest[i] = '\0';
 
-	return (first);
+	return (dest);
 }

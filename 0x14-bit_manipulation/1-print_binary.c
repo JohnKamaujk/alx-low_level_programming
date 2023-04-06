1e47ea9037f522f028ae0fdf6d195c1100521303
JohnKamaujk <johnjkkamau@gmail.com>
2023-04-06 15:31:48 +0300

    :sparkles:print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int limit;
+	int moves;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (limit = n, moves = 0; (limit >>= 1) > 0; moves++)
+
+	for (; moves >= 0; moves--)
+	{
+		if ((n >> moves) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}

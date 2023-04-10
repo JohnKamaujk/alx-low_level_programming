3d6cee3e8479ad45b8004acd2d30e333d82bb1b5
JohnKamaujk <johnjkkamau@gmail.com>
2023-04-10 14:13:09 +0300

    :sparkles:chore:create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
+		while (text_content[length])
+		{
 			length++;
+		}
 	}
 
 	opened = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	writen = write(open, text_content, length);
+	writen = write(opened, text_content, length);
 
 	if (opened == -1 || writen == -1)
 		return (-1);

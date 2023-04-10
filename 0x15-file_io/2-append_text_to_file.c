9cd4802c5be468c9df09424b2ede5646eda8a722
JohnKamaujk <johnjkkamau@gmail.com>
2023-04-10 14:09:33 +0300

    :sparkles:chore:append text files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,24 +11,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int opened, written, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	opened = open(filename, O_WRONLY | O_APPEND);
+	written = write(opened, text_content, length);
 
-	if (o == -1 || w == -1)
+	if (opened == -1 || written == -1)
 		return (-1);
 
-	close(o);
+	close(opened);
 
 	return (1);
 }

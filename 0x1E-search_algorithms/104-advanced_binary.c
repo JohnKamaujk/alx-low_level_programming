487c7709bcbbf3fe80486c65f65b2fbcdc487edb
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-10 12:07:44 +0300

    :sparkles:chore:advanced bnary search algo

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary_recursive - Recursive function advanced binary search
+ * @array: A pointer to the first element of the array to search.
+ * @low: The low index of the search range.
+ * @high: The high index of the search range.
+ * @value: The value to search for.
+ *
+ * Return: The index of the first occurrence of 'value', or -1 if not found.
+ */
+int advanced_binary_recursive(int *array, size_t low, size_t high, int value)
+{
+	if (low > high)
+		return (-1);
+
+	size_t mid = low + (high - low) / 2;
+
+	printf("Searching in array: ");
+	for (size_t i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	if (array[mid] == value)
+	{
+		if (mid == low || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary_recursive(array, low, mid - 1, value));
+	}
+	else if (array[mid] < value)
+		return (advanced_binary_recursive(array, mid + 1, high, value));
+	else
+		return (advanced_binary_recursive(array, low, mid - 1, value));
+}
+
+/**
+ * advanced_binary - Searches a value in a sorted array usin advanced binary.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The index of the first occurrence of 'value', or -1 if not found.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}

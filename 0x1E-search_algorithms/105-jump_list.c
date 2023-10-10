c7980fa1dadc10e381d788cee6371075aa2810c2
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-10 13:22:59 +0300

    :sparkles:chore:jump list search

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -7,7 +7,7 @@
  * @size: The number of nodes in the list.
  * @value: The value to search for.
  *
- * Return: (NULL) If the value is not present or the head of the list is NULL.
+ * Return: If the value is not present or the head of the list is NULL, NULL.
  *         Otherwise, a pointer to the first node where the value is located.
  *
  * Description: Prints a value every time it is compared in the list.
@@ -15,35 +15,37 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump_step, i;
+	size_t jump_step, step_size;
 	listint_t *current, *prev;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	jump_step = sqrt(size);
-	current = list;
-	prev = NULL;
-
-	while (current != NULL && current->n < value)
+	jump_step = 0;
+	step_size = sqrt(size);
+	for (current = prev = list; current->index + 1 < size && current->n < value;)
 	{
 		prev = current;
-		for (i = 0; i < jump_step && current->next != NULL; i++)
-			current = current->next;
-
+		for (jump_step += step_size; current->index < jump_step;
+			current = current->next)
+		{
+			if (current->index + 1 == size)
+				break;
+		}
 		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
 	}
 
-	printf("Value found between indexes [%ld] and [%ld]\n",
-	       prev->index, current->index);
+	printf("Value found between indexes [%ld] and [%ld]\n", prev->index,
+		current->index);
 
-	while (prev != NULL && prev->index <= current->index)
+	while (prev->index < current->index && prev->n < value)
 	{
 		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
 		if (prev->n == value)
 			return (prev);
 		prev = prev->next;
 	}
+	printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
 
-	return (NULL);
+	return ((prev->n == value) ? (prev) : (NULL));
 }
